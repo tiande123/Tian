3b4740fa3fc6df4be9d92332719f88d40d42f19e
984360703 <984360703@qq.com>
2017-01-17 15:42:50 +0800

    first commit

---
+++ Source/Tian/Tian.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tian.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tian, "Tian" );
---
+++ Source/Tian/TianGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TianGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIAN_API ATianGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
